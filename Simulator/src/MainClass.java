f7924f78df10f067833aa7b0e3b82f9e293811b8
albec <albec@MaverickXps>
2016-01-14 17:10:51 +0100

    Primo vero commit

---
+++ Simulator/src/MainClass.java
@@ -0,0 +1,9 @@
+
+import java.sql.SQLException;
+
+public class MainClass {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+		CardLayoutDemo cdld = new CardLayoutDemo();
+	}
+}
